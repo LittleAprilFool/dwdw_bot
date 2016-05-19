03ffc795362f697b1ab819f2a0e72abd7ac0fdea
AprilWong <aprilwang.zju@gmail.com>
2016-05-19 14:34:51 +0800

    lol part finish

--- lol.js
+++ lol.js
@@ -1,34 +1,21 @@
+'use strict'
+
 var request = require('request')
-var http = require('http')
 const url = "http://API.xunjob.cn/"
 
-function getUrl(url) {
-    return http.get(url, (response) => {
-        var body = ''
-        response.on('data', (d) => {
-            body += d
-        })
-        response.on('end', () => {
-            console.log(body)
-            console.log(JSON.parse(body))
-        })
-    })
 
+module.exports = {
+    getRank(playerName, serverName){
+        var rankUrl = url + 's5str.php?' + 'playerName=' + encodeURIComponent(playerName) + '&serverName=' + encodeURIComponent(serverName)
+        return new Promise((resolve, reject)=>{
+            request(rankUrl, (err, res, body) => {
+	        if(!err && res.statusCode == 200) {
+		    var lolRank = JSON.parse(body)
+		    console.log(lolRank)
+		    resolve(lolRank)
+	        }
+    	    })
+        })
+    }
 }
 
-function getRank(){
-    var playerName = 'littleaprilfool'
-    var serverName = '电信十六'
-    var rankUrl = url + 's5str.php?' + 'playerName=' + encodeURIComponent(playerName) + '&serverName=' + encodeURIComponent(serverName)
-    var test = getUrl(rankUrl, null)
-    //console.log(test)
-    //return new Promise((resolve, reject) => {
-    //   request(rankUrl, (err, res, body) => {
-    //        if(!err & res.statusCode == 200){
-    //            console.log(body)
-    //        }
-    //    })
-    //})
-}
-
-getRank()
